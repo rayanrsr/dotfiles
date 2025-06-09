06a85800d454fc186ab1c8a3106303247873b6e2
rayanramoul <r.ramoul@instadeep.com>
2025-06-09 14:04:34 +0200

    build: correclty connect cursor keybind with nvim config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -59,4 +59,4 @@ end, { desc = "Switch to or create tmux session Notes" })
 vim.keymap.set("x", "p", '"_dP', { desc = "Paste without overwriting yank register" })
 
 -- open current buffer in cursor
-vim.keymap.set("n", "<leader>cu", "<cmd>silent !code %<CR>", { desc = "Open current buffer in cursor" })
+vim.keymap.set("n", "<leader>cu", "<cmd>silent !cursor %<CR>", { desc = "Open current buffer in cursor" })
