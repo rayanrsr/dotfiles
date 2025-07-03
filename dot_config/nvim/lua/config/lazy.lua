4a55e2a62e7a405b0773f5f4e7776a7b100f980a
rayanramoul <r.ramoul@instadeep.com>
2025-07-03 22:15:36 +0200

    fix: get rid of the checker

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "rose-pine" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
 
 -- Set transparent background
