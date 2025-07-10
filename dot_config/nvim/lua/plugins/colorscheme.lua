7274e92f62f468e7e02f8ac87f77b93ebaa35267
rayanramoul <r.ramoul@instadeep.com>
2025-07-10 23:30:58 +0200

    feat: update colorscheme to tundra

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,21 +2,21 @@ return {
   {
     -- "folke/tokyonight.nvim",
     -- "nyoom-engineering/oxocarbon.nvim",
-    "rose-pine/neovim",
+    -- "rose-pine/neovim",
+    "sam4llis/nvim-tundra",
     -- "tiagovla/tokyodark.nvim",
-    name = "rose-pine",
+    name = "tundra",
     lazy = false,
     priority = 1000,
     config = function()
-      require("rose-pine").setup({
-        styles = {
-          transparency = true,
-          italic = true,
-          bold = true,
-        },
+      require("nvim-tundra").setup({
+        transparent_background = true,
       })
+
       -- Set the colorscheme
-      vim.cmd("colorscheme rose-pine")
+      vim.cmd("colorscheme tundra")
+      vim.opt.background = "dark"
+      vim.g.tundra_biome = "arctic"
     end,
   },
 }
