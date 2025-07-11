e44d223b98b89bc0f41502f209fdfafdeb5bef07
rayanramoul <r.ramoul@instadeep.com>
2025-07-11 20:30:47 +0200

    feat: change to tokyodark

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,16 @@
 return {
   {
-    -- "folke/tokyonight.nvim",
-    -- "nyoom-engineering/oxocarbon.nvim",
-    -- "rose-pine/neovim",
-    "sam4llis/nvim-tundra",
-    -- "tiagovla/tokyodark.nvim",
-    name = "tundra",
+    "tiagovla/tokyodark.nvim",
+    name = "tokyodark.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("nvim-tundra").setup({
-        transparent_background = true,
-      })
-
-      -- Set the colorscheme
-      vim.cmd("colorscheme tundra")
-      vim.opt.background = "dark"
-      vim.g.tundra_biome = "arctic"
+    opts = {
+      transparent_background = true, -- set background to transparent
+      gamma = 1.00, -- adjust the brightness of the theme
+    },
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      vim.cmd([[colorscheme tokyodark]])
     end,
   },
 }
