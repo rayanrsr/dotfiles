2b85391a79a6980449bbe1a7244416e322870559
rayanramoul <r.ramoul@instadeep.com>
2025-10-23 22:23:18 +0200

    refactor: update colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,17 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    -- "folke/tokyonight.nvim",
     -- "nyoom-engineering/oxocarbon.nvim",
     -- "rose-pine/neovim",
     -- "catppuccin/nvim",
     -- "tiagovla/tokyodark.nvim",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
+      require("kanagawa").setup({
         -- transparent_background = false,
+        transparent = true,
       })
     end,
   },
@@ -17,7 +19,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "kanagawa",
     },
   },
 }
