ecaf5a8a3cb743d7e7df71c9d970519e09acfd52
rayanramoul <r.ramoul@instadeep.com>
2025-11-03 18:50:37 +0100

    refactor: fix aerospace look

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -4,7 +4,14 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME \
+    background.drawing=on \
+    background.color=0xeeffffff \
+    icon.color=0xff000000 \
+    label.color=0xff000000
 else
-  sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME \
+    background.drawing=off \
+    icon.color=0xccffffff \
+    label.color=0xccffffff
 fi
