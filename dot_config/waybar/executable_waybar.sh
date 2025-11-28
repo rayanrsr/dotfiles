4050aba63597fd0872760dbd6e290e65657700ff
rayanramoul <r.ramoul@instadeep.com>
2025-11-28 16:17:25 +0100

    refactor: add support to second screen

--- dot_config/waybar/executable_waybar.sh
+++ dot_config/waybar/executable_waybar.sh
@@ -6,5 +6,6 @@ killall -q waybar
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-# Launch main
-waybar &
+# Launch waybar for each monitor
+waybar -c ~/.config/waybar/config-dp1 &
+waybar -c ~/.config/waybar/config-dp2 &
