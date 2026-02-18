a839e4a887e8b6c5de5694073dee16d10216492e
rayanramoul <r.ramoul@instadeep.com>
2026-02-18 15:58:07 +0100

    feat: new look and wallpapers

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,12 @@
 return {
   -- Configure LazyVim to load tokyodark
   {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-      -- custom options here
-      transparent_background = true,
-    },
+    "rose-pine/neovim",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyodark",
+      colorscheme = "rose-pine",
     },
   },
 }
